0a1b0e076ef519c1e984bd6bdbf62342365071cf
Pablo <pnunfe@gmail.com>
2023-09-04 15:11:58 +0200

    Mouse Follower

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react"
+function App() {
+  const [enable, setEnable] = useState(false)
+  const [position, setPosition] = useState({x: 0, y: 0})
+
+useEffect(() => {
+  console.log('Efecto', {enable})
+  const handleMove = (event) => {
+    const {clientX, clientY} = event
+    setPosition({x: clientX, y:clientY})
+  }
+
+  if (enable) {
+    window.addEventListener('pointermove', handleMove)
+  }
+
+  // Limpiamos efecto
+  return () => {
+    window.removeEventListener('pointermove', handleMove)
+  }
+
+}, [enable])
+
+    return (
+      <main>
+        <h3>Seguimiento de puntero</h3>
+      <div style={
+        {
+          position: 'absolute',
+          background: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: '0.8',
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`,
+        }
+      }>
+        </div>  
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? "Desactivar" : "Activar"} el segumiento
+      </button>
+      </main>
+      )
+}
+
+export default App
